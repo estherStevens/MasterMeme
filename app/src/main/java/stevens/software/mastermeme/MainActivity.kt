2068869d7af7c4cbe04ad694b63a7cd42448f68a
Esther Stevens <estherstevens@Esthers-MacBook-Pro.local>
2024-11-30 18:03:09 +0000

    initial commit

--- app/src/main/java/stevens/software/mastermeme/MainActivity.kt
+++ app/src/main/java/stevens/software/mastermeme/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import stevens.software.mastermeme.ui.theme.MasterMemeTheme
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
